b2f5ecbe903c8b6ada5b131865d09a8ae3e869cc
Lukasz Minarowski <lukasz.minarowski@gmail.com>
2026-04-27 17:33:19 +0200

    Delete cga-local-app directory

--- cga-local-app/frontend/src/api/client.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({ baseURL: 'http://localhost:8000' })
-
-export const setToken = (token: string) => {
-  api.defaults.headers.common.Authorization = `Bearer ${token}`
-}
